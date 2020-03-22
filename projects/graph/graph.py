15d9b19d13f0ba5bc1972920013b3dcf571de505
Lisa Wilton <mishmao14@gmail.com>
2020-03-22 16:04:16 +0000

    questions on earliest ancestor

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -174,7 +174,7 @@ class Graph:
                     s.push(next_path)
         return None            
 
-    def dfs_recursive(self, starting_vertex):
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -182,7 +182,15 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+
+        # if visited is None:
+        #     visited = set()
+
+        # # base case
+        # if starting_vertex == destination_vertex:
+            # return path
+
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
@@ -209,7 +217,7 @@ if __name__ == '__main__':
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
-    print(graph.vertices)
+    # print(graph.vertices)
 
     '''
     Valid BFT paths:
@@ -226,7 +234,7 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    graph.bft(1)
+    # graph.bft(1)
 
     '''
     Valid DFT paths:
@@ -235,8 +243,8 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
-    graph.dft(1)
-    graph.dft_recursive(1)
+    # graph.dft(1)
+    # graph.dft_recursive(1)
 
     '''
     Valid BFS path:
